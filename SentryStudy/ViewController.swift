ab91c4232304b03e6eae04116b6085db5b0ed6d2
Hailey <min_i0212@hanmail.net>
2020-03-19 10:51:40 +0900

    First Commit

---
+++ SentryStudy/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  SentryStudy
+//
+//  Created by Hailey on 2020/03/19.
+//  Copyright © 2020 min_e. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func ErrorButton(_ sender: UIButton) {
+        
+    }
+    
+}
+
