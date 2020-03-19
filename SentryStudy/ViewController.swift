279c9484d3c35aacf5fb27d076dbdb3e82f6438b
Hailey <min_i0212@hanmail.net>
2020-03-19 11:49:20 +0900

    Update - make custom event

--- SentryStudy/ViewController.swift
+++ SentryStudy/ViewController.swift
@@ -17,7 +17,21 @@ class ViewController: UIViewController {
     }
 
     @IBAction func errorSend(_ sender: UIButton) {
-        Client.shared?.crash()
+        let param: [String: String] = [
+            "name": "min_e",
+            "context": "I'm hungry."]
+        
+        let message = param.map { $0.key + " = " + $0.value }.joined(separator: "\n")
+        let event = Event(level: .debug)
+        let exception = Exception(value: message, type: "exception")
+        event.exceptions = [exception]
+        let frame = Frame()
+        frame.fileName = #file
+        frame.function = #function
+        frame.lineNumber = #line
+        let stacktace = Stacktrace(frames: [frame], registers: ["name": "Min"])
+        event.stacktrace = stacktace
+        Client.shared?.send(event: event)
     }
 }
 
